90b1d8550b4cbb0788eb4affd4e639ec8b733000
Carlos Arévalo <45299446+carlosv20@users.noreply.github.com>
2020-03-13 12:35:33 -0500

    Add files via upload

---
+++ Market/Webprueba/Webprueba/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webprueba.Models
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+    }
+}
